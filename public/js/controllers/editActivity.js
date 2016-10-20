7d9ed9169ccc7607f96fd8cbde483786709da302
medaak <medaakdev@gmail.com>
2016-10-20 17:27:06 +0200

    Ajout et modification des controllers

--- public/js/controllers/editActivity.js
+++ public/js/controllers/editActivity.js
@@ -1,3 +1,16 @@
 class editActivity {
-    constructor() {}
+    constructor(activiteService) {
+      this.activiteService = activiteService;
+      this.load();
+    }
+    load() {
+        this.activiteService.getAll().then((res) => {
+            this.activites = res.data;
+        })
+    }
+    update(activite) {
+        this.activiteService.update(activite._id, activite.logoActivite, activite.titreActivite, activite.descriptionActivite).then(() => {
+            this.load()
+        })
+    }
 }
