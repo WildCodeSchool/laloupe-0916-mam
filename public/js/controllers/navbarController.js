9b2b7ae11e4aa4db26a586a313d83af3ee3b142a
medaak <medaakdev@gmail.com>
2016-10-21 17:01:01 +0200

    Ajout des modifs de gaelle navbar responsive

--- public/js/controllers/navbarController.js
+++ public/js/controllers/navbarController.js
@@ -12,9 +12,11 @@ class navbarController {
               this.user = sessionFactory.user;
           })
           */
+        $(".button-collapse").sideNav();
+
         $('.js-scrollTo').on('click', function() { // Au clic sur un élément
             var page = $(this).attr('href'); // Page cible
-            var speed = +(window.scrollY+$(page).offset().top)/2; // Durée de l'animation (en ms)
+            var speed = +(window.scrollY + $(page).offset().top) / 2; // Durée de l'animation (en ms)
             $('html, body').animate({
                 scrollTop: ((window.scrollY < 324) ? ($(page).offset().top - 170 - 64) : ($(page).offset().top - 170))
             }, speed); // Go
