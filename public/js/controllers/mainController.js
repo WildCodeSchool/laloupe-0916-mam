d1cc3dd85332fb3da94155cefd34a54354d3f385
Jipsian <sylvain.labarre@live.fr>
2016-11-03 16:44:42 +0100

    techno toujours pareille, boom boom dans les oreilles

--- public/js/controllers/mainController.js
+++ public/js/controllers/mainController.js
@@ -46,25 +46,4 @@ class mainController {
             this.partenaires = res.data;
         })
     }
-
-    /*
-    create() {
-        this.todoService.create(this.todo).then(() => {
-            this.todo = '';
-            this.load()
-        })
-    }
-
-    update(todo) {
-        this.todoService.update(todo._id, todo.description).then(() => {
-            this.load()
-        })
-    }
-
-    delete(todo) {
-        this.todoService.delete(todo._id).then(() => {
-            this.load()
-        })
-    }
-*/
 }
