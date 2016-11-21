9ea38bbcd5189e9be03afff5aa90093c90243703
Gaëlle Morin <morin.gaelle@gmail.com>
2016-11-21 11:53:03 +0100

    Update createPartenaire.js

--- public/js/controllers/createPartenaire.js
+++ public/js/controllers/createPartenaire.js
@@ -1,41 +1,37 @@
-class createPartenaire {
-    constructor(partenaireService) {
-        this.partenaireService = partenaireService;
-        this.partenaire = {}
-        this.load();
+function createPartenaire(partenaireService) {
+    this.partenaireService = partenaireService;
+    this.partenaire = {}
 
-        function uploadFile(file) {
-            var url = '/api/picture';
-            var xhr = new XMLHttpRequest();
-            var fd = new FormData();
-            xhr.open("POST", url, true);
-            xhr.onreadystatechange = function() {
-                if (xhr.readyState == 4 && xhr.status == 200) {
-                    // Every thing ok, file uploaded
-                    console.log(xhr.responseText); // handle response.
-                }
-            };
-            fd.append("upload_file", file);
-            xhr.send(fd);
-        };
-
-        var uploadfiles = document.querySelector('#uploadImage');
-        uploadfiles.addEventListener('change', function() {
-            var files = this.files;
-            for (var i = 0; i < files.length; i++) {
-                uploadFile(this.files[i]); // call the function to upload the file
+    function uploadFile(file) {
+        var url = '/api/picture';
+        var xhr = new XMLHttpRequest();
+        var fd = new FormData();
+        xhr.open("POST", url, true);
+        xhr.onreadystatechange = function() {
+            if (xhr.readyState == 4 && xhr.status == 200) {
+                // Every thing ok, file uploaded
+                console.log(xhr.responseText); // handle response.
             }
-        }, false);
+        };
+        fd.append("upload_file", file);
+        xhr.send(fd);
+    };
 
-    }
+    var uploadfiles = document.querySelector('#uploadImage');
+    uploadfiles.addEventListener('change', function() {
+        var files = this.files;
+        for (var i = 0; i < files.length; i++) {
+            uploadFile(this.files[i]); // call the function to upload the file
+        }
+    }, false);
 
-    load() {
+    const load = () => {
         this.partenaireService.getAll().then((res) => {
             this.partenaires = res.data;
         })
     }
 
-    create(partenaire) {
+    const create = (partenaire) => {
         var urlImage = '/uploads/img_' + document.getElementById('uploadImage').value.split(/(\|\/)/g).pop().replace('C:\\fakepath\\', '');
         console.log(urlImage);
         partenaire.logoPartenaire = urlImage;
@@ -49,9 +45,10 @@ class createPartenaire {
         })
     }
 
-    delete(partenaire) {
+    const delete = (partenaire) => {
         this.partenaireService.delete(partenaire._id, partenaire.logoPartenaire, partenaire.titrePartenaire, partenaire.descriptionPartenaire).then(() => {
             this.load()
         })
     }
+    this.load();
 }
