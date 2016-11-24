dec59f7f0744171125fdfb8b19eb30e826be0c13
Gaëlle Morin <morin.gaelle@gmail.com>
2016-11-24 13:00:55 +0100

    Update editActivity.js
    MàJ Firefox

--- public/js/controllers/editActivity.js
+++ public/js/controllers/editActivity.js
@@ -1,14 +1,13 @@
-class editActivity {
-    constructor(activiteService) {
+function editActivity (activiteService) {
         this.activiteService = activiteService;
         this.load();
-    }
-    load() {
+    
+    load = () => {
         this.activiteService.getAll().then((res) => {
             this.activites = res.data;
         })
     }
-    update(activite) {
+    update = (activite) => {
         this.activiteService.update(activite._id, activite.logoActivite, activite.titreActivite, activite.descriptionActivite).then(() => {
             this.load()
         })
